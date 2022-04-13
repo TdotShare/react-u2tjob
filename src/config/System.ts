2f4bd5c6febe8a0930a2b704b30600ae305dfe00
Jirayu Ch <52649379+TdotShare@users.noreply.github.com>
2022-04-13 16:58:42 +0700

    update config
    update config

--- src/config/System.ts
+++ src/config/System.ts
@@ -1,7 +1,7 @@
 export const systemConfig  =  {
-    ImagesPath : process.env.NODE_ENV === 'production' ? "http://127.0.0.1:8000/upload" : "https://u2tjob.ird.rmuti.ac.th/service/u2tjob/upload" , 
-    HOST : process.env.NODE_ENV === 'production' ? "http://127.0.0.1:3000" : "https://u2tjob.ird.rmuti.ac.th" , 
-    API : process.env.NODE_ENV === 'production' ? "http://127.0.0.1:8000/api" : "https://u2tjob.ird.rmuti.ac.th/service/u2tjob/api" , 
+    ImagesPath : process.env.NODE_ENV !== 'production' ? "http://127.0.0.1:8000/upload" : "https://u2tjob.ird.rmuti.ac.th/service/u2tjob/upload" , 
+    HOST : process.env.NODE_ENV !== 'production' ? "http://127.0.0.1:3000" : "https://u2tjob.ird.rmuti.ac.th" , 
+    API : process.env.NODE_ENV !== 'production' ? "http://127.0.0.1:8000/api" : "https://u2tjob.ird.rmuti.ac.th/service/u2tjob/api" , 
     NameFull : `ระบบสมัครงาน U2TJob IRD`,
     NameInit : `IRD - U2TJob`,
     BaseRouter : process.env.NODE_ENV !== 'production' ? "/" : "/" 
